2b133bf0fc431b29af785bda5b8cea86f3cb6ccb
phosphene47 <phosphene47@chiya.dev>
2020-08-23 16:25:46 +1000

    Move assets from external source into repository

--- _includes/index.js
+++ _includes/index.js
@@ -1,53 +1,53 @@
-"use strict";
+"use strict"
 
 // enable tooltips
-$("#social").children().tooltip();
+$("#social").children().tooltip()
 
-var time = new Date().getHours();
-var bgUrl;
+var time = new Date().getHours()
+var bgUrl
 
 // 10pm to 6am
 if (time >= 22 || time < 6)
-  bgUrl = "https://images8.alphacoders.com/789/789587.png";
+  bgUrl = "/assets/images/night.jpg"
 
 // 6am to 2pm
 else if (time < 14)
-  bgUrl = "https://images5.alphacoders.com/752/752727.png";
+  bgUrl = "/assets/images/morning.jpg"
 
 // 2pm to 10pm
 else if (time < 22)
-  bgUrl = "https://images7.alphacoders.com/789/789589.png";
+  bgUrl = "/assets/images/noon.jpg"
 
 // load background image
 $("<img/>").attr("src", bgUrl).on("load", function () {
-  $(this).remove();
+  $(this).remove()
 
   // fade in background
   $("#background")
     .css("background-image", "url(" + bgUrl + ")")
-    .fadeIn("slow");
+    .fadeIn("slow")
 
   // fade in logo
   $("#logo")
     .on("load", function () {
-      $(this).delay(300).fadeIn();
-      $("#social").delay(300).fadeIn();
+      $(this).delay(300).fadeIn()
+      $("#social").delay(300).fadeIn()
     }).each(function () {
       if (this.complete)
-        $(this).trigger("load");
-    });
-});
+        $(this).trigger("load")
+    })
+})
 
 // load logo
 $("#logo")
-  .attr("src", "/assets/images/chiya-logo.png");
+  .attr("src", "/assets/images/chiya-logo.png")
 
 // social media fading
 $("#social")
   .children("a").each(function () {
     $(this).hover(function () {
-      $(this).stop(true).css("opacity", 1);
+      $(this).stop(true).css("opacity", 1)
     }, function () {
-      $(this).stop(true).fadeTo("fast", 0.5);
-    });
-  });
+      $(this).stop(true).fadeTo("fast", 0.5)
+    })
+  })
